ab88be76e523a048f9b700a0a53abcb39235cb86
Florian Progri <florian.progri@ikubinfo.al>
2024-06-10 12:12:34 +0200

    mochawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
   e2e: {
     baseUrl: "https://demo.nopcommerce.com",
     retries: {
@@ -11,9 +12,9 @@ module.exports = defineConfig({
     viewportWidth: 1400,
     viewportHeight: 800,
     video: false,
-    reporter: "spec",
     // experimentalRunAllSpecs: true,
     setupNodeEvents(on, config) {
+      require("cypress-mochawesome-reporter/plugin")(on);
       // implement node event listeners here
     },
   },
